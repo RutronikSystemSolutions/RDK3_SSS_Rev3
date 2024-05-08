27fc87c413841fe4796b4426ee414a60ebefc36f
Rutronik Elektronische Bauelemente GmbH <46448552+RutronikKaunas@users.noreply.github.com>
2024-05-08 16:08:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -113,7 +113,7 @@ int main(void)
     }
 
     /* Start Thermal Sensor Task */
-    xTaskCreate(ThermalSensorTask, "thermal task", configMINIMAL_STACK_SIZE*2, NULL, configMAX_PRIORITIES - 4, &ThermalSensorTaskHandle);
+    xTaskCreate(ThermalSensorTask, "thermal task", configMINIMAL_STACK_SIZE*3, NULL, configMAX_PRIORITIES - 4, &ThermalSensorTaskHandle);
     if(ThermalSensorTaskHandle == NULL)
     {
     	printf("Error: could not create thermal task.\r\n");
