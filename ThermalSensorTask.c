89dc9811dd864ba5e92bf07e5bd24bd8b999fe64
Rutronik Elektronische Bauelemente GmbH <46448552+RutronikKaunas@users.noreply.github.com>
2024-05-10 10:22:16 +0300

    thermal imaging process recovery implemented

--- ThermalSensorTask.c
+++ ThermalSensorTask.c
@@ -15,9 +15,14 @@
 #include "SmartDisplayTask.h"
 #include "d6t32l01a.h"
 
-#define D6T_I2C_FREQ	1000000UL
+#define D6T_I2C_FREQ			1000000UL
+#define D6T_PROCESS_TIMEOUT_MS	500
+
+static void ImageReadProcess(void *param);
 
 TaskHandle_t ThermalSensorTaskHandle = NULL;
+TaskHandle_t ImageReadProcessHandle = NULL;
+TickType_t img_process_timestamp = 0;
 
 /*D6T32L01A Variables*/
 uint8_t rbuf[N_READ];
@@ -37,8 +42,6 @@ void ThermalSensorTask(void *param)
 {
 	(void) param;
     cy_rslt_t result;
-    uint32_t d6t_rslt;
-    uint32_t ulPreviousValue;
 
     /*Initialize I2C Master*/
     result = cyhal_i2c_init(&I2C_scb1, D6T_SDA, D6T_SCL, NULL);
@@ -59,16 +62,63 @@ void ThermalSensorTask(void *param)
     	CY_ASSERT(0);
     }
 
+	/*Initialize current time*/
+	TickType_t currentTime = xTaskGetTickCount()* (1000/configTICK_RATE_HZ);
+
+	for(;;)
+	{
+		/*If the thermal image is not being read - start the process*/
+		if(ImageReadProcessHandle == NULL)
+		{
+		    /* Create Image Processing Task */
+		    xTaskCreate(ImageReadProcess, "img read task", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 4, &ImageReadProcessHandle);
+		    if(ImageReadProcessHandle == NULL)
+		    {
+		    	printf("Error: could not d6t image processing task.\r\n");
+		    	CY_ASSERT(0);
+		    }
+		    vTaskDelay(pdMS_TO_TICKS(1000));
+		}
+		else
+		{
+			vTaskDelay(pdMS_TO_TICKS(100));
+			currentTime = xTaskGetTickCount()* (1000/configTICK_RATE_HZ);
+
+			/*Delete the process if it holds too long*/
+			if((currentTime - img_process_timestamp) >  D6T_PROCESS_TIMEOUT_MS)
+			{
+				vTaskDelete(ImageReadProcessHandle);
+				ImageReadProcessHandle = NULL;
+				vTaskDelay(pdMS_TO_TICKS(100));
+			}
+		}
+	}
+}
+
+static void ImageReadProcess(void *param)
+{
+    uint32_t d6t_rslt;
+    uint32_t ulPreviousValue;
+
+	/*Reset the sensor*/
+	cyhal_gpio_write((cyhal_gpio_t)ARDU_ADC3, false);
+	vTaskDelay(pdMS_TO_TICKS(100));
+	cyhal_gpio_write((cyhal_gpio_t)ARDU_ADC3, true);
+	vTaskDelay(pdMS_TO_TICKS(400));
+
     /*Initialize the D6T32L01A */
     vTaskDelay(pdMS_TO_TICKS(400));
     d6t_rslt = d6t32_init();
     if(d6t_rslt != D6T_OK)
     {
     	printf("D6T32L01 Initialization Failure.\r\n");
-    	CY_ASSERT(0);
+    	for(;;)
+    	{vTaskDelay(pdMS_TO_TICKS(1000));}
     }
 
     memset(rbuf, 0x00, sizeof(rbuf));
+    img_process_timestamp = xTaskGetTickCount()* (1000/configTICK_RATE_HZ);
+    printf("D6T32L01 image reading process started at: %d\r\n", (int)img_process_timestamp);
 
 	for(;;)
 	{
@@ -81,19 +131,16 @@ void ThermalSensorTask(void *param)
         	/*Check if the temperature values were read without any errors*/
             if(d6t_rslt != D6T_OK)
             {
-            	/*Try to recover*/
-            	memset(rbuf, 0x00, sizeof(rbuf));
-            	cyhal_gpio_write((cyhal_gpio_t)ARDU_ADC3, false);
-            	vTaskDelay(pdMS_TO_TICKS(100));
-            	cyhal_gpio_write((cyhal_gpio_t)ARDU_ADC3, true);
-            	vTaskDelay(pdMS_TO_TICKS(400));
-            	d6t32_init();
+            	for(;;)
+            	{vTaskDelay(pdMS_TO_TICKS(1000));}
             }
+
+            /*Read the current time indicating the image process is active*/
+            img_process_timestamp = xTaskGetTickCount()* (1000/configTICK_RATE_HZ);
     	}
     	else
     	{
     		vTaskDelay(pdMS_TO_TICKS(50));
-    		cyhal_gpio_toggle(LED3);
     	}
 	}
 }
